a92c7a479a88f17bce6c6c8f975048755eb0da24
Devharsh Trivedi <dtrivedi@bowiestate.edu>
2025-02-03 12:06:29 -0500

    Create LargerNumber.java

---
+++ LargerNumber.java
@@ -0,0 +1,18 @@
+// Write your name here
+
+import java.util.Scanner;
+
+public class LargerNumber {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Write your code here...
+      
+
+        System.out.println("The larger number is: " + largerNumber);
+
+        input.close(); // Close the scanner to release resources (good practice)
+    }
+}
